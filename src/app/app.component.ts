46618d202edf9c17562ae867d111ea7504bf7d33
KAVI <kavindu123450@gmail.com>
2023-03-24 12:25:57 +0530

    48

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,40 @@ export class AppComponent {
 
   title = 'Flexiart Company';
 
+  private subscription: Subscription | undefined;
+  public dateNow = new Date();
+  public dDay = new Date('august 01 2023 00:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+
+  public timeDifference:any;
+  public secondsToDday:any;
+  public minutesToDday:any;
+  public hoursToDday:any;
+  public daysToDday:any;
+
+  private getTimeDifference () {
+    this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+    this.allocateTimeUnits(this.timeDifference);
+  }
+
+  private allocateTimeUnits (timeDifference: any) {
+    this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+    this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+    this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+    this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+  }
+
   constructor() {
   }
 
   ngOnInit(): void {
     this.buttonClick()
+
+    this.subscription = interval(1000)
+      .subscribe(x => { this.getTimeDifference(); });
   }
 
   buttonClick(){
