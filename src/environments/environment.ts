563c8c729f136e5704fdc173b7a8d0a3202cfea9
KAVI <kavindu123450@gmail.com>
2023-03-28 11:55:19 +0530

    66

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   dialogflow:{
     projectId:"flexiart-chatbot-gtqa"
   },
-  baseUrl:"http://3.111.167.233:3214/api/v1/"
+  baseUrl:"https://3.111.167.233:3214/api/v1/"
   // baseUrl:"http://localhost:3214/api/v1/"
 };
 
