7f1b10d6a440d38e8f143c7b287d324eccfed8cf
KAVI <kavindu123450@gmail.com>
2023-06-21 00:36:36 +0530

    add meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {interval, Subscription} from "rxjs";
-import { Meta } from "@angular/platform-browser";
+import {Meta} from "@angular/platform-browser";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'Flexiart Company';
 
@@ -17,49 +18,85 @@ export class AppComponent {
   milliSecondsInASecond = 1000;
   hoursInADay = 24;
   minutesInAnHour = 60;
-  SecondsInAMinute  = 60;
+  SecondsInAMinute = 60;
 
-  public timeDifference:any;
-  public secondsToDday:any;
-  public minutesToDday:any;
-  public hoursToDday:any;
-  public daysToDday:any;
+  public timeDifference: any;
+  public secondsToDday: any;
+  public minutesToDday: any;
+  public hoursToDday: any;
+  public daysToDday: any;
 
-  private getTimeDifference () {
-    this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+  isDropdownActive: boolean = false;
+
+  private getTimeDifference() {
+    this.timeDifference = this.dDay.getTime() - new Date().getTime();
     this.allocateTimeUnits(this.timeDifference);
   }
 
-  private allocateTimeUnits (timeDifference: any) {
+  private allocateTimeUnits(timeDifference: any) {
     this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
     this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
     this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
     this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
   }
 
-  constructor(private meta: Meta) { }
+  constructor(private meta: Meta, private router: Router) {
+  }
 
   ngOnInit(): void {
-    this.meta.updateTag({ name: 'title', content: 'Flexiart Pvt.Ltd - Make your digital dreams come true' });
-    this.meta.updateTag({ name: 'viewport', content: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' });
-    this.meta.updateTag({ name: 'author', content: 'flexiart' });
-    this.meta.updateTag({ name: 'robots', content: 'none' });
-    this.meta.updateTag({ httpEquiv: 'X-UA-Compatible', content: 'ie=edge' });
-    this.meta.updateTag({ name: 'description', content: 'we are help to grow customer\'s businesses faster than ever. we are helps to customers\n' +
-        '  with developing mobile and web applications' });
-    this.meta.updateTag({ name: 'keywords', content: 'web development, mobile development, software development, web application, mobile apps, software company,\n' +
+    this.meta.updateTag({name: 'title', content: 'Flexiart Pvt.Ltd - Make your digital dreams come true'});
+    this.meta.updateTag({
+      name: 'viewport',
+      content: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'
+    });
+    this.meta.updateTag({name: 'author', content: 'flexiart'});
+    this.meta.updateTag({name: 'robots', content: 'none'});
+    this.meta.updateTag({httpEquiv: 'X-UA-Compatible', content: 'ie=edge'});
+    this.meta.updateTag({
+      name: 'description',
+      content: 'we are help to grow customer\'s businesses faster than ever. we are helps to customers\n' +
+        '  with developing mobile and web applications'
+    });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'web development, mobile development, software development, web application, mobile apps, software company,\n' +
         '        best software development company, app development company, node development, mean stack, mern stack, flexible apps,\n' +
-        '        flexiart, digital flexi, web app developer, mobile app developer, flexi-art' });
+        '        flexiart, digital flexi, web app developer, mobile app developer, flexi-art'
+    });
 
     this.buttonClick()
 
     this.subscription = interval(1000)
-      .subscribe(x => { this.getTimeDifference(); });
+      .subscribe(x => {
+        this.getTimeDifference();
+      });
+
+    this.isDropdownActive = false;
   }
 
-  buttonClick(){
-    const btn= document.getElementById('main-model-btn');
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
+
+  onDropdownItemClicked() {
+    this.isDropdownActive = true;
+    this.onNavItemClicked();
+  }
+
+  buttonClick() {
+    const btn = document.getElementById('main-model-btn');
     // @ts-ignore
     btn.click();
+  }
+
+  closeNavbar() {
+    if (document.querySelector('.navbar-collapse.show')) {
+      const navbarToggler = document.querySelector('.navbar-toggler');
+      (navbarToggler as HTMLElement).click();
+    }
+  }
+
+  onNavItemClicked() {
+    this.closeNavbar();
   }
 }
