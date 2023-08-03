f06bf198c7cc2e1dc601e61a7288755f8fb4be9e
KAVI <kavindu123450@gmail.com>
2023-08-03 21:19:53 +0530

    email 1

--- src/app/components/dashbord/email-template-editor/email-template-editor.component.ts
+++ src/app/components/dashbord/email-template-editor/email-template-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import DOMPurify from 'dompurify';
 
 @Component({
   selector: 'app-email-template-editor',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./email-template-editor.component.scss']
 })
 export class EmailTemplateEditorComponent {
+
+  htmlContent: string = '';
+  renderedHtml: SafeHtml = '';
+  view: boolean = false;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  convertHtmlToPage() {
+    const sanitizedHtml = DOMPurify.sanitize(this.htmlContent);
+    this.renderedHtml = this.sanitizer.bypassSecurityTrustHtml(sanitizedHtml);
+    this.view ? this.view = false : this.view = true;
+  }
 
 }
